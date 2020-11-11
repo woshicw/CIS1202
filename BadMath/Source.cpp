6abb23ea246678c1b7ea294762117b7e93bbab16
Wei <wwww@wwwcom>
2020-11-10 21:19:44 -0500

    Change Third number input n2 to n3.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	float sum = i1 + i2 + i3;
+	return  sum  / 3;
 }
 
 int main()
@@ -13,9 +14,9 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
-	float a = average(n1, n2, n3);
+	float a = average(n1,n2,n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
